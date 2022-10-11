694e92d6aad70a86765020c4f1b331d24d6f3654
zhuchentong <2037630@163.com>
2022-10-11 17:14:23 +0800

    feat(custom): 更新git action支持

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,13 +15,16 @@ module.exports = {
   deploy: {
     production: {
       user: 'root', //ssh 登陆服务器用户名
-      host: '', //ssh 地址服务器domain/IP
+      host: '121.5.165.149', //ssh 地址服务器domain/IP
       ref: 'origin/master', //Git远程/分支
-      repo: '', //git地址使用ssh地址
-      path: '', //项目存放服务器文件路径
+      repo: 'git@github.com:gopowerteam-mall/mall-service.git', //git地址使用ssh地址
+      path: '/home/apps/mall-service', //项目存放服务器文件路径
       ssh_options: 'StrictHostKeyChecking=no',
       'pre-deploy': 'git fetch --all',
-      'post-deploy': 'yarn && yarn build && pm2 reload ecosystem.config.js', //部署后的动作
+      'post-deploy':
+        'pnpm install \
+        && pnpm run build \
+        && pm2 reload ecosystem.config.js', //部署后的动作
       env: {
         NODE_ENV: 'production',
       },
