d0a06fd8a350997e1e4fc4e6554b45c826963434
zhuchentong <2037630@163.com>
2022-10-11 18:00:16 +0800

    fix(custom): 更新github action

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       user: 'root', //ssh 登陆服务器用户名
       host: '121.5.165.149', //ssh 地址服务器domain/IP
       ref: 'origin/master', //Git远程/分支
-      repo: 'https://github.com/gopowerteam-mall/mall-service.git', //git地址使用ssh地址
+      repo: 'git@github.com:gopowerteam-mall/mall-service.git', //git地址使用ssh地址
       path: '/home/apps/mall-service', //项目存放服务器文件路径
       ssh_options: 'StrictHostKeyChecking=no',
       'pre-deploy': 'git fetch --all',
